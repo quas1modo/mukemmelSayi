ea9638cd4bb5973c90904ade1049c9389c020f6f
quas1modo <75099936+quas1modo@users.noreply.github.com>
2023-05-11 15:23:06 +0300

    Update Test.java

--- java101/src/Test.java
+++ java101/src/Test.java
@@ -4,18 +4,21 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
 
-        // Değişkenler oluşturuldu, Koşulların sağlanması için minimum sayıya maksimum, maksimum sayıya ise minimum sayı atandı.
+       
         int count = 0, number;
 
         Scanner input = new Scanner(System.in);
         System.out.print("Kaç tane sayı gireceksiniz: ");
         number = input.nextInt();
-
+        
+        // Döngüde eğer girilen sayının çarpanı bulunursa count değişkenine ekleniyor.
         for (int i = 1; i < number; i++) {
             if (number % i == 0) {
                 count += i;
             }
         }
+        
+        // Eğer count toplamı sayının kendisine eşitse çıktı mükemmel sayıdır oluyor.
         if (count == number) {
             System.out.println(number + " Mükemmel sayıdır.");
         } else {
